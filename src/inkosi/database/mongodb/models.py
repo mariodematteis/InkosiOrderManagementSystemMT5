46441e5f964031c232bba659fd29c5419b0a53ea
BopaxDev <marionicdematteis@gmail.com>
2023-10-06 15:47:52 +0200

    Add models for MongoDB

---
+++ src/inkosi/database/mongodb/models.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+from inkosi.models.log import LogType
+
+
+@dataclass
+class Log:
+    PackageName: str
+    ModuleName: str
+    Level: LogType
+    Service: str
+    Message: str
