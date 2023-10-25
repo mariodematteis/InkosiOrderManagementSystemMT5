266e34ea78ff37fe8bbc63f3339794dd8a1dbad5
BopaxDev <marionicdematteis@gmail.com>
2023-10-25 12:08:16 +0200

    Update api package

--- src/inkosi/api/v1/_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .endpoints.admin.scheduler import router as scheduler_router
-
-v1_router = APIRouter()
-v1_router.include_router(
-    scheduler_router,
-    tags=["Scheduler"],
-)
