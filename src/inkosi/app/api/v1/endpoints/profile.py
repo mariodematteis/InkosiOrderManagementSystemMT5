1d7bbee1148097715d844f8986c97b77c01ab21a
BopaxDev <marionicdematteis@gmail.com>
2023-10-26 17:34:37 +0200

    Fix bug

--- src/inkosi/app/api/v1/endpoints/profile.py
+++ src/inkosi/app/api/v1/endpoints/profile.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Response, status
 from fastapi.responses import JSONResponse
 
 from inkosi.database.postgresql.database import PostgreSQLCrud
-from inkosi.database.postgresql.models import LoginCredentials
+from inkosi.database.postgresql.schemas import LoginCredentials, UserRole
 from inkosi.log.log import Logger
 
 router = APIRouter()
@@ -14,14 +14,17 @@ logger = Logger(module_name="Profile", package_name="api")
 async def login(credentials: LoginCredentials) -> Response:
     postgresql = PostgreSQLCrud()
     records = postgresql.get_users(
-        email_address=credentials.EmailAddress, password=credentials.Password
+        email_address=credentials.email_address,
+        password=credentials.password,
     )
 
     match len(records):
         case 0:
             logger.warn(
-                "Unable to locate the user with the following E-mail Address:"
-                f" {credentials.EmailAddress}"
+                message=(
+                    "Unable to locate the user with the following E-mail Address:"
+                    f" {credentials.email_address}"
+                )
             )
 
             return JSONResponse(
@@ -33,11 +36,25 @@ async def login(credentials: LoginCredentials) -> Response:
                 status_code=status.HTTP_401_UNAUTHORIZED,
             )
         case 1:
+            if not UserRole.has(key=records[0].role):
+                logger.warn(
+                    message=(
+                        "Unable to identify the role of user with the following ID and"
+                        f" E-mail Address respectively: {records[0].id} and"
+                        f" {records[0].email_address}"
+                    )
+                )
+                return JSONResponse(
+                    content={
+                        "detail": "Unable to identify the role",
+                    },
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                )
+
             return JSONResponse(
                 content={
-                    "detail": (
-                        "Unable to locate the user through the E-mail Address given"
-                    )
+                    "detail": "Successfully logged in",
+                    "role": records[0].role,
                 },
                 status_code=status.HTTP_200_OK,
             )
@@ -72,7 +89,7 @@ async def login(credentials: LoginCredentials) -> Response:
             logger.critical(
                 message=(
                     "More than two users have been found with the following E-mail"
-                    f" Address: {credentials.EmailAddress}"
+                    f" Address: {credentials.email_address}"
                 )
             )
             return JSONResponse(
