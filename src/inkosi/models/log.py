4fb48fac8e29d380e9edf7efb2fed8a977f800f2
BopaxDev <marionicdematteis@gmail.com>
2023-10-27 12:55:30 +0200

    Remove models package

--- src/inkosi/models/log.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class LogType(StrEnum):
-    CRITICAL: str = "CRITICAL"
-    DEBUG: str = "DEBUG"
-    ERROR: str = "ERROR"
-    INFO: str = "INFORMATION"
-    WARN: str = "WARNING"
