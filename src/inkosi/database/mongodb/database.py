0bf5765bc9c09a48fca673adb1e78d8f7a6bd38c
BopaxDev <marionicdematteis@gmail.com>
2023-10-27 12:11:05 +0200

    Implement CRUD Operations on Trades

--- src/inkosi/database/mongodb/database.py
+++ src/inkosi/database/mongodb/database.py
@@ -1,10 +1,20 @@
+from dataclasses import asdict
+
 import certifi
-from pymongo import MongoClient
+from bson import ObjectId
+from pymongo import MongoClient, ReturnDocument
+from pymongo.collection import Collection
 from pymongo.database import Database
 
 from inkosi.log.log import Logger
 from inkosi.utils.exceptions import MongoConnectionError
-from inkosi.utils.settings import get_mongodb_settings, get_mongodb_url
+from inkosi.utils.settings import (
+    get_mongodb_collection,
+    get_mongodb_settings,
+    get_mongodb_url,
+)
+
+from .schemas import TradeRequest
 
 logger = Logger(module_name="MongoDBDatabase", package_name="mongodb", database=False)
 
@@ -83,3 +93,113 @@ class MongoDBInstance(
         self,
     ) -> None:
         self.client.close()
+
+
+class MongoDBCrud:
+    def __init__(self) -> None:
+        self.mongodb_instance = MongoDBInstance()
+
+    def add_trade(
+        self,
+        trade_request: TradeRequest,
+    ) -> ObjectId:
+        trades_collection = self.mongodb_instance.database[
+            get_mongodb_collection().Trade
+        ]
+
+        result = trades_collection.insert_one(asdict(trade_request))
+        return result.inserted_id
+
+    def update_trade(
+        self,
+        trade_id: ObjectId | str,
+        updates: TradeRequest,
+    ) -> dict | None:
+        trades_collection: Collection = self.mongodb_instance.database[
+            get_mongodb_collection().Trade
+        ]
+
+        result: dict | None = trades_collection.find_one_and_update(
+            {
+                "_id": trade_id
+                if isinstance(trade_id, ObjectId)
+                else ObjectId(trade_id),
+            },
+            {
+                "$set": asdict(
+                    updates,
+                    dict_factory=lambda x: {k: v for (k, v) in x if v is not None},
+                ),
+            },
+            return_document=ReturnDocument.AFTER,
+        )
+        return result
+
+    def remove_trade(
+        self,
+        trade_id: ObjectId | str,
+    ) -> bool:
+        trades_collection: Collection = self.mongodb_instance.database[
+            get_mongodb_collection().Trade
+        ]
+
+        result = trades_collection.delete_one(
+            {
+                "_id": trade_id
+                if isinstance(trade_id, ObjectId)
+                else ObjectId(trade_id),
+            },
+        )
+        return result.deleted_count == 1
+
+    def get_trade(
+        self,
+        trade_id: ObjectId | str,
+    ) -> dict:
+        trades_collection: Collection = self.mongodb_instance.database[
+            get_mongodb_collection().Trade
+        ]
+
+        if isinstance(trade_id, str):
+            trade_id = ObjectId(trade_id)
+
+        result = list(
+            trades_collection.aggregate(
+                [
+                    {
+                        "$match": {
+                            "_id": trade_id,
+                        },
+                    },
+                    {
+                        "$addFields": {
+                            "_id": {
+                                "$toString": "$_id",
+                            },
+                        },
+                    },
+                ]
+            )
+        )
+        return result[0] if len(result) == 1 else {}
+
+    def get_all_trades(
+        self,
+    ) -> list[dict]:
+        trades_collection: Collection = self.mongodb_instance.database[
+            get_mongodb_collection().Trade
+        ]
+
+        return list(
+            trades_collection.aggregate(
+                [
+                    {
+                        "$addFields": {
+                            "_id": {
+                                "$toString": "$_id",
+                            },
+                        },
+                    },
+                ]
+            )
+        )
