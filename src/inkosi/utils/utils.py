243343a80aed5e49c9c310aff9d4c58113943468
BopaxDev <marionicdematteis@gmail.com>
2023-11-04 18:18:27 +0100

    Add method to EnhancedStrEnum

--- src/inkosi/utils/utils.py
+++ src/inkosi/utils/utils.py
@@ -5,3 +5,7 @@ class EnhancedStrEnum(StrEnum):
     @classmethod
     def has(cls, key: str):
         return key in cls.__members__.values()
+
+    @classmethod
+    def list(cls):
+        return list(cls.__members__.values())
