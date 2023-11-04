5798215ffb4e84c1c3058c327f39e48809ab5139
BopaxDev <marionicdematteis@gmail.com>
2023-11-04 18:21:21 +0100

    Add new endpoint

--- src/inkosi/app/api/v1/endpoints/admin/operations.py
+++ src/inkosi/app/api/v1/endpoints/admin/operations.py
@@ -1,3 +1,35 @@
-from fastapi import APIRouter
+import random
+import string
+from hashlib import sha256
+
+from fastapi import APIRouter, Request, Response
+
+from inkosi.app.schemas import Mode
+from inkosi.database.postgresql.database import PostgreSQLCrud
+from inkosi.database.postgresql.models import Authentication
 
 router = APIRouter()
+
+
+@router.post(path="/generate_token_backtest")
+async def generate_token_backtest(
+    request: Request,
+) -> Response:
+    postgresql = PostgreSQLCrud()
+
+    token_id: str = sha256(
+        "".join(
+            [
+                random.choice(list(set(string.ascii_uppercase).union(string.digits)))
+                for _ in range(16)
+            ]
+        ).encode()
+    ).hexdigest()
+
+    authentication = Authentication(
+        id=token_id,
+        ip_address=request.client.host,
+        mode=Mode.BACKTEST,
+    )
+
+    postgresql.postgresql_instance.add(model=[authentication])
