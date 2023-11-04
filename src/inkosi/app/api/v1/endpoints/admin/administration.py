d656d3cd7fe6b2dd7acf486b4c7e54ac5131c514
BopaxDev <marionicdematteis@gmail.com>
2023-11-04 18:21:33 +0100

    Add new endpoints

--- src/inkosi/app/api/v1/endpoints/admin/administration.py
+++ src/inkosi/app/api/v1/endpoints/admin/administration.py
@@ -7,20 +7,32 @@ from inkosi.app.schemas import AdministratorRequest, Returns
 from inkosi.database.postgresql.database import PostgreSQLCrud, PostgreSQLInstance
 from inkosi.database.postgresql.models import Administrator
 from inkosi.database.postgresql.schemas import (
+    AddAdministratorToFund,
+    AddInvestorToFund,
     AdministratorProfile,
+    Commission,
     Fund,
     PoliciesUpdate,
     ReturnRequest,
 )
+from inkosi.utils.settings import get_api_settings
 
 router = APIRouter()
+
+
+@router.get(path="/available_apis")
+async def return_available_apis() -> Response:
+    return JSONResponse(
+        content={"detail": get_api_settings().APIs},
+        status_code=status.HTTP_200_OK,
+    )
 
 
 @router.post(
     path="/create_administrator",
     response_model=list[AdministratorProfile],
 )
-def create_administrator(administrator_request: AdministratorRequest):
+async def create_administrator(administrator_request: AdministratorRequest):
     postgres_instance = PostgreSQLInstance()
 
     administrator = Administrator(
@@ -46,7 +58,7 @@ def create_administrator(administrator_request: AdministratorRequest):
     path="/list_portfolio_managers",
     response_model=list[AdministratorProfile],
 )
-def list_portfolio_managers(status: bool | None = None):
+async def list_portfolio_managers(status: bool | None = None):
     """
     Return the list of Active, Inactive or all Portfolio Managers
 
@@ -72,7 +84,7 @@ def list_portfolio_managers(status: bool | None = None):
     path="/list_funds",
     response_model=list[Fund],
 )
-def list_funds(status: bool | None = None):
+async def list_funds(status: bool | None = None):
     """
     Return the list of Active, Inactive or all Funds
 
@@ -94,7 +106,7 @@ def list_funds(status: bool | None = None):
 
 
 @router.post(path="/returns", response_model=Returns)
-def returns(return_request: ReturnRequest):
+async def returns(return_request: ReturnRequest):
     ...
 
 
@@ -102,7 +114,7 @@ def returns(return_request: ReturnRequest):
     path="/update_policies",
     response_class=Response,
 )
-def update_policies(policies_update: PoliciesUpdate):
+async def update_policies(policies_update: PoliciesUpdate):
     postgres = PostgreSQLCrud()
     result = postgres.update_policies(
         policies_update=policies_update,
@@ -122,3 +134,76 @@ def update_policies(policies_update: PoliciesUpdate):
         },
         status_code=status.HTTP_200_OK,
     )
+
+
+@router.post(path="/add_investor")
+async def add_investor(add_investor_to_fund: AddInvestorToFund) -> Response:
+    postgres = PostgreSQLCrud()
+    result = postgres.add_investor_to_fund(
+        add_investor_to_fund.investor_id,
+        add_investor_to_fund.fund,
+    )
+
+    if result:
+        return JSONResponse(
+            content={
+                "detail": "Investor correctly added to the fund",
+            },
+            status_code=status.HTTP_200_OK,
+        )
+    else:
+        return JSONResponse(
+            content={
+                "detail": "Unable to add the investor to the fund",
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+@router.post(path="/add_administrator")
+async def add_administrator(
+    add_administrator_to_fund: AddAdministratorToFund,
+) -> Response:
+    postgres = PostgreSQLCrud()
+    result = postgres.add_administrator_to_fund(
+        add_administrator_to_fund.administrator_id,
+        add_administrator_to_fund.fund,
+    )
+
+    if result:
+        return JSONResponse(
+            content={
+                "detail": "Administrator correctly added to the fund",
+            },
+            status_code=status.HTTP_200_OK,
+        )
+    else:
+        return JSONResponse(
+            content={
+                "detail": "Unable to add the administrator to the fund",
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+@router.put(path="/update_commission")
+async def update_commission(
+    commission: Commission,
+) -> Response:
+    postgres = PostgreSQLCrud()
+    result = postgres.update_commission(commission=commission)
+
+    if result:
+        return JSONResponse(
+            content={
+                "detail": "Commission information correctly updated",
+            },
+            status_code=status.HTTP_200_OK,
+        )
+    else:
+        return JSONResponse(
+            content={
+                "detail": "Unable to update the commission information",
+            },
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
