b482f2d8d058d734378b3a0e5eb780644745104f
BopaxDev <marionicdematteis@gmail.com>
2023-11-05 12:05:13 +0100

    Added IntEnum

--- src/inkosi/api/schemas.py
+++ src/inkosi/api/schemas.py
@@ -1,9 +1,22 @@
 from dataclasses import dataclass
+from enum import IntEnum
+
+
+class StatusTradeResult(IntEnum):
+    ORDER_FILLED: int = 0
+    NO_TICKER_PROVIDED: int = -1
+    TICKER_NOT_FOUND: int = -2
+    NO_VOLUME_PROVIDED: int = -3
+    NO_OPERATION_SPECIFIED: int = -4
+    NO_TAKE_PROFIT_SPECIFIED: int = -5
+    NO_STOP_LOSS_SPECIFIED: int = -6
+    NO_ORDER_FILLING: int = -7
 
 
 @dataclass
 class OpenRequestTradeResult:
+    trade_id: int
     detail: str
-    status: int
+    status: StatusTradeResult
     error: str | None = None
     error_code: int | None = None
